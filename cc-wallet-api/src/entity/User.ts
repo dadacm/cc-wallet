61e562b302017d550da0241a2c7c49b83ecb075d
DADA <davimarque2000@gmail.com>
2021-12-02 19:45:35 -0300

    cria endpoints

--- cc-wallet-api/src/entity/User.ts
+++ cc-wallet-api/src/entity/User.ts
@@ -1,8 +1,14 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from "typeorm";
+import { Count } from "./count";
 
 @Entity()
 export class User {
 
+    constructor(name: string, email: string) {
+        this.name = name;
+        this.email = email;
+    }
+    
     @PrimaryGeneratedColumn()
     id: number;
 
@@ -11,5 +17,8 @@ export class User {
 
     @Column()
     email: string;
+
+    @OneToMany(() => Count, count => count.user)
+    count: Count[];
 
 }
